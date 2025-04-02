812ba6cc1d74df50426bb6f7f2168ad0dba973c3
anhthu258 <k.anhthu02@gmail.com>
2025-04-02 12:38:18 +0200

    program tgt

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+    dodger.style.backgroundColor = "#FF69B4";
+    dodger.style.bottom = "0px";
+    dodger.style.left = "0px";
+
+    document.addEventListener("keydown", function (event) {
+        console.log(event);
+      });
