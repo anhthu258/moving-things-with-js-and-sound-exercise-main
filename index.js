be91fcb709a6865723ca395ee3e1bf0d141cedba
anhthu258 <k.anhthu02@gmail.com>
2025-04-02 13:11:43 +0200

    Tilføjet op og ned

--- index.js
+++ index.js
@@ -5,23 +5,45 @@ function moveDodgerLeft() {
   const left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.left = `${left - 10}px`; //Flytter dodger til venstre med 10px
+    dodger.style.left = `${left - 10}px`; // Flytter dodger 10px til venstre
   }
 }
 
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) { //påstår at dodgeren er 40px bred
-      dodger.style.left = `${left + 10}px`; // Flytter dodger til højre med 10px
-    }
-  }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    } else if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
+  if (left < 360) { // Påstår containerens bredde er 400px da dodgerens bredde er 40px, dette limit dodger til at ikke at flytte ud af containeren
+    dodger.style.left = `${left + 10}px`; // Flytte dodgeren 10px til højre
+  }
+}
+
+function moveDodgerUp() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom < 380) { // Limiter dodger til at ikke at flytte ud af containeren
+    dodger.style.bottom = `${bottom + 10}px`; // Flytter dodger 10px oppe
+  }
+}
+
+function moveDodgerDown() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 10}px`; // Flyttere dodger 10px ned
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  } else if (e.key === "ArrowUp") {
+    moveDodgerUp();
+  } else if (e.key === "ArrowDown") {
+    moveDodgerDown();
+  }
 });
